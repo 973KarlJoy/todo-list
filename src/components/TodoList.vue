cfb01f010bbb33c5ee531045a6280f7fd2f79565
karl joyeux <karl.joyeux@efrei.net>
2022-11-11 20:13:53 +0100

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TodoForm from "@/components/TodoForm";
+import TodoList from "@/components/TodoList";
+</script>
+
+<template>
+  <div class="todoList">
+    <h1>Here is my Todo List !</h1>
+    <todo-form> </todo-form>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<style></style>
---
+++ src/stores/counter.js
@@ -0,0 +1,11 @@
+
+import { defineStore } from "pinia";
+
+export const useTodoListStore = defineStore("todoList", {
+  //State
+  state: () => ({
+    todoList: [],
+  }),
+  //Getters
+  //action
+})
