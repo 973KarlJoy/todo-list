054ad6dc3f7bf4708edd597200cb0dc20d9b3682
karl joyeux <karl.joyeux@efrei.net>
2022-11-11 20:23:19 +0100

    creer 2 actions : ajouter et enlever un item

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -7,4 +7,14 @@ export const useTodoListStore = defineStore("todoList", {
   }),
   //Getters
   //action
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false });
+    },
+    deletTodo(item) {
+      this.todoList = this.todoList.filter((object) =>{
+        return object.id !== itemId
+      });
+    },
+  },
 });
